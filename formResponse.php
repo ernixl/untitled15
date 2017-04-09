5e4004701d9fcd0ab974ea936917b7e2bbddbcdd
ernixl <ernixl@yahoo.com>
2017-04-09 21:32:16 +0100

    yes

---
+++ formResponse.php
@@ -0,0 +1,13 @@
+<! DOCTYPE html >
+<html>
+<head>
+    <?php
+    $forename = $_POST["forename"];
+    $surname = $_POST["surname"];
+     echo $forename . $surname;
+?>
+</head>
+<body>
+
+</body>
+</html>
