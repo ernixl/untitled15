dc20433a58debe73a61bb9c2bbdec98a511b8def
ernixl <ernixl@yahoo.com>
2017-04-09 21:34:57 +0100

    yes

--- formResponse.php
+++ formResponse.php
@@ -4,8 +4,8 @@
     <?php
     $forename = $_POST["forename"];
     $surname = $_POST["surname"];
-     echo "$forename" . "". "$surname";
-?>
+    echo "$forename" . "" . "$surname";
+    ?>
 </head>
 <body>
 
