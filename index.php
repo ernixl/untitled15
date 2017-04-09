c482904774ed5827e5948bf38678ab6be4500e57
ernixl <ernixl@yahoo.com>
2017-04-09 21:47:30 +0100

    yes

--- index.php
+++ index.php
@@ -10,13 +10,22 @@
 
     <form action="formResponse.php" method="post"
 </form>
+<p>
 Name: <input type="text" name="name"><br>
+</p>
+<p>
 Surnname: <input type="text" name="surname"><br>
+</p>
+<p>
 Main Superhero Power: <input type="text" name="superhero"><br>
+</p>
+<p>
 Gender: <input type="radio" name="gender" value="female">female
         <input type="radio" name="gender" value="male">male<br>
-
+</p>
+<p>
 <input type="submit" value="Submit">
+</p>
 </form>
 </body>
 </html>
