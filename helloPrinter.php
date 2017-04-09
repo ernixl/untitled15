1d3f6f45b83d15dc26d8341741b517c1cd033736
ernixl <ernixl@yahoo.com>
2017-04-09 20:36:13 +0100

    Initial commit

---
+++ helloPrinter.php
@@ -0,0 +1,9 @@
+<! DOCTYPE ht ml >
+<html>
+<head>
+    Hello Earth!
+</head>
+<body>
+
+</body>
+</html>
---
+++ helloselector.php
@@ -0,0 +1,18 @@
+<! DOCTYPE ht ml >
+<html>
+    <head>
+    </head>
+    <body>
+    <p>
+        <a href="helloPrinter.php">Earth</a>
+    </p>
+    <p>
+        <a href="helloPrinter.php">Mars</a>
+    </p>
+    <p>
+        <a href="helloPrinter.php">Uranus</a>
+    </p>
+    </body>
+</html>
+
+
